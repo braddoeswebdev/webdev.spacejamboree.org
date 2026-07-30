1dd7ad5919c8017e04749e07f034ec64b00fb0ce
Brad Thompson <bkt@brad-thompson.com>
2026-07-29 21:49:24 -0400

    updates!

--- app/models/completion.rb
+++ app/models/completion.rb
@@ -5,7 +5,26 @@ class Completion < ApplicationRecord
   delegate :user, to: :participation
   has_rich_text :artifacts
 
+  scope :blank, -> {
+    left_joins(:rich_text_artifacts)
+      .where("action_text_rich_texts.id IS NULL OR COALESCE(action_text_rich_texts.body, '') = ''")
+  }
+  scope :in_progress, -> {
+    left_joins(:rich_text_artifacts)
+      .where("action_text_rich_texts.id IS NOT NULL AND COALESCE(action_text_rich_texts.body, '') != ''")
+      .where(complete: false)
+  }
+  scope :complete, -> { where(complete: true) }
+
   before_create :populate_artifacts
+
+  def blank?
+    artifacts.blank?
+  end
+
+  def in_progress?
+    !blank? && !complete?
+  end
 
   private
 
