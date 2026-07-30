0ec95a991aa5952751303beb266b540543be4854
Brad Thompson <bkt@brad-thompson.com>
2026-07-29 22:52:29 -0400

    actually fix completion counts maybe

--- app/controllers/workshops_controller.rb
+++ app/controllers/workshops_controller.rb
@@ -30,11 +30,7 @@ class WorkshopsController < ApplicationController
   # GET /workshops/1/review
   def review
     @badges = Badge.includes(:requirements).all
-    @completion_counts = @workshop.completions.joins(:participation)
-      .where(participation: { workshop_id: @workshop.id })
-      .group(:requirement_id)
-      .select("requirement_id, COUNT(*) AS total, SUM(CASE WHEN complete THEN 1 ELSE 0 END) AS completed_count")
-      .index_by(&:requirement_id)
+    @completions = @workshop.completions
   end
 
   # GET /workshops/1/review/:requirement_id
